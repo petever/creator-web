92f848d5f965f4a42470acf84a5510b75c5e863b
김승훈 <rlatmdgns94@naver.com>
2024-09-03 22:26:54 +0900

    feat: msw setup (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Button, Container, Grid, Input} from "@mantine/core";
+import { Button, Container, Input } from "@mantine/core";
 
 export const metadata = {
   title: "크리에이터 구독",
@@ -7,7 +7,7 @@ export const metadata = {
 export default function Page() {
   return (
     <Container>
-      <Input size="md" radius="md" placeholder="Login" />
+      <Input size="md" radius="md" placeholder="Login"/>
       <Button variant="filled" color="lime" size='md' fullWidth>LOGIN</Button>
     </Container>
   );
---
+++ src/mocks/handlers/home.ts
@@ -0,0 +1,8 @@
+import { http, HttpResponse, } from 'msw'
+import { postsMockData } from "../model";
+
+export const postsHandler = [
+  http.get('/posts', () => {
+    return HttpResponse.json(postsMockData)
+  }),
+]
