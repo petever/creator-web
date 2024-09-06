694c22785b1349d77f1e5fbb6386933ff1952e1c
김승훈 <rlatmdgns94@naver.com>
2024-09-06 09:46:36 +0900

    feat: react-query setup

--- src/app/layouts/index.tsx
+++ src/app/layouts/index.tsx
@@ -1,18 +1,21 @@
-import { Flex, MantineProvider } from '@mantine/core'
+import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import { Sidebar } from '@/widgets/Sidebar'
 import { ThemeToggleButton } from '@/widgets/ThemeToggleButton'
+import QueryProvider from '@/app/providers/QueryProvider'
 
 export function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
-        <MantineProvider defaultColorScheme="dark">
-          <ThemeToggleButton />
-          <Sidebar isLogin={false} />
-          {/*<MSWComponent />*/}
-          {children}
-        </MantineProvider>
+        <QueryProvider>
+          <MantineProvider defaultColorScheme="dark">
+            <ThemeToggleButton />
+            <Sidebar isLogin={false} />
+            {/*<MSWComponent />*/}
+            {children}
+          </MantineProvider>
+        </QueryProvider>
       </body>
     </html>
   )
