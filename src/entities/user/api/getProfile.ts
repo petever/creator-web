047bc5bbc7358bbf97b852d0fc37648dba7299f9
김승훈 <rlatmdgns94@naver.com>
2024-09-25 13:57:29 +0900

    test: getProfile

--- src/entities/user/api/getProfile.ts
+++ src/entities/user/api/getProfile.ts
@@ -1,6 +1,18 @@
 import { apiService } from '@/shared/core/apiService'
 import { API_URL } from '@/shared/constants/apiURL'
+import { getSession } from 'next-auth/react'
 
 export const getProfile = async () => {
   return apiService.get(API_URL.USERS_PROFILE).json()
+}
+
+export const testGetProfile = async () => {
+  const session = await getSession()
+  return fetch(`http://182.227.155.8:9090/users/profile`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${session?.accessToken}`,
+    },
+  })
 }
--- src/entities/user/hooks/useMy.ts
+++ src/entities/user/hooks/useMy.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
 import { QUERY_KEY } from '@/shared/constants/queryKey'
-import { getProfile } from '@/entities/user/api/getProfile'
+import { testGetProfile } from '@/entities/user/api/getProfile'
 
 export const useMyProfile = () => {
   const { data } = useQuery({
     queryKey: [QUERY_KEY.MY_PROFILE],
-    queryFn: () => getProfile(),
+    queryFn: () => testGetProfile(),
   })
 
   console.log(data, 'data')
