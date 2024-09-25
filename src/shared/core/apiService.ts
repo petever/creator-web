0fbbde41fd92fe06c8bfa034457186395a353af7
김승훈 <rlatmdgns94@naver.com>
2024-09-25 13:45:12 +0900

    test: login

--- src/shared/core/apiService.ts
+++ src/shared/core/apiService.ts
@@ -1,6 +1,5 @@
 import ky from '@toss/ky'
 import { auth } from '@/auth'
-import { getSession } from 'next-auth/react'
 
 export const apiService = ky.create({
   prefixUrl: 'http://182.227.155.8:9090',
@@ -8,11 +7,7 @@ export const apiService = ky.create({
   hooks: {
     beforeRequest: [
       async (request: Request) => {
-        if (typeof window === 'undefined') {
-          const session = await auth()
-          request.headers.set('Authorization', `Bearer ${session?.accessToken}`)
-        }
-        const session = await getSession()
+        const session = await auth()
         request.headers.set('Authorization', `Bearer ${session?.accessToken}`)
       },
     ],
