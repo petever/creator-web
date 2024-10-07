510a4b944d5a02781c4a984db0189a4a5d60ab61
김승훈 <rlatmdgns94@naver.com>
2024-10-07 21:28:55 +0900

    test: post upload

--- src/widgets/AddContentModal/ui/index.tsx
+++ src/widgets/AddContentModal/ui/index.tsx
@@ -1,11 +1,10 @@
-import { Modal} from '@mantine/core'
+import { Modal } from '@mantine/core'
 import { ContentFormProvider, useContentForm } from '@/widgets/AddContentModal/lib/form-context'
-import ContentUpload from "@/widgets/AddContentModal/ui/ContentUpload";
-import {ContentForm} from "@/widgets/AddContentModal/ui/ContentForm";
-import {AddContentFooter} from "@/widgets/AddContentModal/ui/AddContentFooter";
-import {Simulate} from "react-dom/test-utils";
-import {useCreatePosts} from "@/widgets/AddContentModal/hooks/useCreatePosts";
-
+import ContentUpload from '@/widgets/AddContentModal/ui/ContentUpload'
+import { ContentForm } from '@/widgets/AddContentModal/ui/ContentForm'
+import { AddContentFooter } from '@/widgets/AddContentModal/ui/AddContentFooter'
+import { Simulate } from 'react-dom/test-utils'
+import { useCreatePosts } from '@/widgets/AddContentModal/hooks/useCreatePosts'
 
 interface AddContentModalProps {
   opened: boolean
@@ -18,48 +17,42 @@ const AddContentModal = ({ opened, onClose }: AddContentModalProps) => {
   const form = useContentForm({
     mode: 'controlled',
     initialValues: {
-      currentFile : '',
-      currentFileType : 'image',
-      currentIndex : 0,
-			step : 0,
+      currentFile: '',
+      currentFileType: 'image',
+      currentIndex: 0,
+      step: 0,
       title: '',
       contents: '',
-			files : [],
-      isPreview : false,
-      isSubscribed : 'false'
+      files: [],
+      isPreview: false,
+      isSubscribed: 'false',
     },
   })
 
   const { onSubmit, reset } = form
 
   const handleSubmit = (values: typeof form.values) => {
-    const { title, contents, isSubscribed, files} = values
-    const payload = {
-      postRequest : {
-        title,
-        contents,
-        isSubscribed : values.isSubscribed !== 'false',
-      },
-      files
-    }
+    const { title, contents, isSubscribed, files } = values
 
     const formData = new FormData()
     formData.append(
       'postRequest',
       new Blob(
         [
-          JSON.stringify(payload.postRequest),
+          JSON.stringify({
+            title,
+            contents,
+            isSubscribed: values.isSubscribed !== 'false',
+          }),
         ],
         { type: 'application/json' },
       ),
     )
 
-    files.forEach((file, index) => {
-      formData.append(`files`, file);
-    });
+    // formData.append(`files`, new Blob(files))
 
     createPostMutation(formData)
-  };
+  }
 
   const handleModalClose = () => {
     reset()
@@ -67,12 +60,12 @@ const AddContentModal = ({ opened, onClose }: AddContentModalProps) => {
   }
 
   return (
-    <Modal size='xl' centered opened={opened} onClose={handleModalClose} title="새 게시물 만들기">
+    <Modal size="xl" centered opened={opened} onClose={handleModalClose} title="새 게시물 만들기">
       <ContentFormProvider form={form}>
         <form onSubmit={onSubmit(handleSubmit)}>
           <ContentUpload />
-          <ContentForm/>
-          <AddContentFooter/>
+          <ContentForm />
+          <AddContentFooter />
         </form>
       </ContentFormProvider>
     </Modal>
