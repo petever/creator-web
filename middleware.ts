b3355d5279cc7607a6f40c8b231350306dd35f1e
김승훈 <rlatmdgns94@naver.com>
2024-10-11 10:48:24 +0900

    fix: 로그인 버그 수정

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import NextAuth from 'next-auth'
 import { NextRequest, NextResponse } from 'next/server'
 import authConfig from '@/auth.config'
+import { auth as userSession } from '@/auth'
 import { PAGE } from '@/shared/constants/page'
 
 const { auth } = NextAuth(authConfig)
 export default auth(async function middleware(req: NextRequest) {
-  const session = await auth()
+  const session = await userSession()
   if (!session?.accessToken) return NextResponse.redirect(new URL(PAGE.LOGIN, req.url))
 })
 
