3e399b00af3c8b89500296fa9fc81931fcbc2fe7
김승훈 <rlatmdgns94@naver.com>
2024-10-17 17:09:16 +0900

    feat: 구독 UI 작업중

--- src/pageModules/subscribe/SubscribePlanSetting.tsx
+++ src/pageModules/subscribe/SubscribePlanSetting.tsx
@@ -1,17 +1,17 @@
 'use client'
 import React from 'react'
-import { Flex, Input } from '@mantine/core'
+import { Box, Flex, Input } from '@mantine/core'
 import Editor from '@/widgets/Editor'
 
 const SubscribePlanSetting = () => {
   return (
-    <div>
+    <Box bd="1px solid ">
       <Flex gap={10} direction="column">
-        <Input />
-        <Input />
-        <Editor />
+        <Input placeholder="구독 플랜 제목" />
+        <Input placeholder="가격" />
+        <Editor placeholder="플랜 설명" />
       </Flex>
-    </div>
+    </Box>
   )
 }
 
