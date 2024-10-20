0a3255a8d7215ac2c6506dbcda3e05512a72e43e
shinbom <tlsqha@gmail.com>
2024-10-20 20:04:00 +0900

    style : login

--- src/pageModules/login/LoginPage.tsx
+++ src/pageModules/login/LoginPage.tsx
@@ -1,38 +1,45 @@
 import { signIn } from '@/auth'
+import {Button, Container, Flex, Text} from "@mantine/core";
+import Image from 'next/image'
 
 const LoginPage = () => {
   return (
     <>
       {/*<ThemeToggleButton />*/}
-      {/*<Container>*/}
-      {/*  <Flex mih={50} gap="md" justify="center" direction="column" align="center" wrap="wrap">*/}
-      {/*    <Text size="xl" fw={700}>*/}
-      {/*      로그인*/}
-      {/*    </Text>*/}
-      {/*    <Button*/}
-      {/*      w={300}*/}
-      {/*      h={50}*/}
-      {/*      type="button"*/}
-      {/*      variant="white"*/}
-      {/*      color="dark"*/}
-      {/*      leftSection={*/}
-      {/*        <Image src="/assets/logo-google.svg" alt="Google" width={20} height={20} />*/}
-      {/*      }*/}
-      {/*      onClick={() => signIn('google')}*/}
-      {/*    >*/}
-      {/*      <Text size="xl" fw={700}>*/}
-      {/*        구글로 시작하기*/}
-      {/*      </Text>*/}
-      {/*    </Button>*/}
-      {/*  </Flex>*/}
-      {/*</Container>*/}
       <form
         action={async () => {
           'use server'
           await signIn('google')
         }}
       >
-        <button type="submit">Signin with Google</button>
+        <Container>
+          <Flex h={'100vh'} gap="md" justify="center" direction="column" align="center" wrap="wrap">
+            <Text
+              size="100px"
+              fw={700}
+              mb={'100px'}
+              variant="gradient"
+              gradient={{ from: 'teal', to: 'lime', deg: 190 }}
+            >
+              Shoo
+            </Text>
+            <Button
+              w={300}
+              h={50}
+              type="submit"
+              variant="white"
+              color="dark"
+              leftSection={
+                <Image src="/assets/logo-google.svg" alt="Google" width={20} height={20} />
+              }
+              // onClick={() => signIn('google')}
+            >
+              <Text size="xl" fw={700}>
+                구글로 시작하기
+              </Text>
+            </Button>
+          </Flex>
+        </Container>
       </form>
     </>
   )
