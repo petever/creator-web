9789481d182a10bab28d9e573762546960e7fe9f
김승훈 <rlatmdgns94@naver.com>
2024-10-24 19:42:25 +0900

    style: 추천 리스트

--- src/shared/RecommentList/ui/RecommendList.tsx
+++ src/shared/RecommentList/ui/RecommendList.tsx
@@ -1,41 +1,18 @@
 'use client'
-import { Title } from '@mantine/core'
-import classes from './styles.module.css'
-import { useMediaQuery } from '@mantine/hooks'
 import { UserProfile } from '@/entities/user/types'
 import { Profile } from '@/features'
-import { useRouter } from 'next/navigation'
-import { PAGE } from '@/shared/constants/page'
+import { Box } from '@mantine/core'
 
 interface RecommendListProps {
   recommends: UserProfile[]
 }
 
 export const RecommendList = ({ recommends }: RecommendListProps) => {
-  const isPcWidth = useMediaQuery('(min-width: 1200px)')
-
-  const router = useRouter()
-
-  const goToMemberPage = (name: string) => {
-    router.push(PAGE.MEMBER_PAGE(name))
-  }
-
-  if (!isPcWidth) return null
-
   return (
-    <div className={classes.wrapper}>
-      <Title order={4} className={classes.title}>
-        회원님을 위한 추천
-      </Title>
-      <ul className={classes.recommend}>
-        {recommends.map((user) => {
-          return (
-            <li key={user.id}>
-              <Profile profile={user} size={'lg'} isSubscribed />
-            </li>
-          )
-        })}
-      </ul>
-    </div>
+    <Box w="320px" mt={20}>
+      {recommends.map((user) => {
+        return <Profile key={user.id} profile={user} size={'lg'} isSubscribed />
+      })}
+    </Box>
   )
 }
