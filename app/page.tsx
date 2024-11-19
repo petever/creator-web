8d079762113f86dac4418896e7b2440168bae204
김승훈 <rlatmdgns94@naver.com>
2024-11-19 23:39:41 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,12 @@
-export { default } from '@/pageModules/main/MainPage'
+export const metadata = {
+  title: '크리에이터 구독',
+}
+
+export default async function Page() {
+  return (
+    <div className="flex">
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
