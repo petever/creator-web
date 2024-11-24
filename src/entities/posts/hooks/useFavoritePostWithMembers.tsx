044ee89c7c2942415b7425048a2caf353a4978db
shinbom <tlsqha@gmail.com>
2024-11-25 01:09:53 +0900

    feat : 포스팅 좋아요 멤버조회 개발

--- src/entities/posts/hooks/useFavoritePostWithMembers.tsx
+++ src/entities/posts/hooks/useFavoritePostWithMembers.tsx
@@ -1,30 +1,30 @@
-import { QueryClient, useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
-import { getFeeds } from '@/entities/feeds/api/getFeeds'
-import {getFeedQueryKey, getFeedSearchParams, getMoreFeeds} from '@/entities/feeds/lib'
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
+import {QUERY_KEY} from "@/shared/constants/queryKey";
 import {getFavoritePostWithMembers} from "@/entities/posts/api/getFavoritePostWithMember";
 
-const useFeeds = (username: string) => {
+const useFavoritePostWithMembers = (postingId: string) => {
+	const params = {
+		size: 10,
+	}
   const queryClient = useQueryClient()
   return useInfiniteQuery({
-    queryKey: getFeedQueryKey(username),
-    queryFn: getFeeds,
+    queryKey: [QUERY_KEY.FEED_LIKE_MEMBERS, postingId],
+    queryFn: ({pageParam = params}) => getFavoritePostWithMembers(postingId, params),
     initialData: {
       pages: [],
-      pageParams: [],
+      pageParams: [params],
     },
-    initialPageParam: getFeedSearchParams(username),
+    initialPageParam: params,
     getNextPageParam: (lastPage) => {
       const lastIndex = lastPage.content.length - 1
       if(lastPage.content.length < 10) return
-      const nextId = lastPage.content[lastIndex]?.id
-      queryClient.getQueryData(getFeedQueryKey(username))
+      // const nextId = lastPage.content[lastIndex]?.id
+      queryClient.getQueryData([QUERY_KEY.FEED_LIKE_MEMBERS, postingId])
       return {
         size: 10,
-        sort: ['string'],
-        nextId,
       }
     },
   })
 }
 
-export default useFeeds
+export default useFavoritePostWithMembers
