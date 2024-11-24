d2486074a4da7bf0a1a7df48b6e5777bf8ca9a92
김승훈 <rlatmdgns94@naver.com>
2024-11-25 02:29:05 +0900

    feat: next-auth 수정

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,11 +2,7 @@ import type { NextAuthConfig } from 'next-auth'
 import Google from 'next-auth/providers/google'
 
 export default {
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
+  providers: [Google],
+  secret: process.env.AUTH_SECRET,
+  trustHost: true,
 } satisfies NextAuthConfig
