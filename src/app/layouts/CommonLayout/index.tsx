78d1b122ce01572763ef9c0032ce738a14788c1c
김승훈 <rlatmdgns94@naver.com>
2024-12-04 18:43:41 +0900

    style: common layout 수정

--- src/app/layouts/CommonLayout/index.tsx
+++ src/app/layouts/CommonLayout/index.tsx
@@ -1,7 +1,7 @@
 import { HeaderNavigation } from '@/shared/ui/HeaderNavigation'
 import { SidebarProvider } from '@/shared/ui/sidebar'
 import { AppSidebar } from '@/shared/ui/AppSidebar/ui'
-import {BottomNavigation} from "@/shared/ui/BottomNavigation";
+import { BottomNavigation } from '@/shared/ui/BottomNavigation'
 
 interface SidebarLayoutProps {
   children: React.ReactNode
@@ -9,15 +9,15 @@ interface SidebarLayoutProps {
 
 export async function CommonLayout({ children }: SidebarLayoutProps) {
   return (
-    <>
+    <div className="max-w-screen-xl mx-auto">
       <HeaderNavigation />
       <SidebarProvider>
-        <div className="flex flex-1">
+        <div className="flex flex-1 ">
           <AppSidebar />
-          <div className="flex flex-1 justify-center py-20">{children}</div>
+          <div className="flex flex-1 justify-center">{children}</div>
         </div>
       </SidebarProvider>
-      <BottomNavigation/>
-    </>
+      <BottomNavigation />
+    </div>
   )
 }
--- src/shared/ui/AppSidebar/ui/index.tsx
+++ src/shared/ui/AppSidebar/ui/index.tsx
@@ -1,19 +1,18 @@
 'use client'
 import { Sidebar } from '@/shared/ui/sidebar'
-import { CirclePlus, Compass, Home, Search } from 'lucide-react'
 
 import { SidebarContentWrapper, SidebarFooterWrapper, SidebarHeaderWrapper } from '@/entities'
-import {useIsMobile} from "@/shared/hooks/use-mobile";
+import { useIsMobile } from '@/shared/hooks/use-mobile'
 
 interface AppSidebarProps {}
 
 export const AppSidebar = ({}: AppSidebarProps) => {
   const isMobile = useIsMobile()
 
-  if(isMobile) return null
+  if (isMobile) return null
 
   return (
-    <Sidebar>
+    <Sidebar style={{ left: 'inherit' }}>
       <SidebarHeaderWrapper />
       <SidebarContentWrapper />
       <SidebarFooterWrapper />
