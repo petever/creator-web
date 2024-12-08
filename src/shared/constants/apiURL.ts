92f6521cd0b06447b980d4aac92b03cdf69d7d6a
shinbom <61568434+shinbom@users.noreply.github.com>
2024-12-08 21:51:05 +0900

    feat : 회원 탈퇴 (#64)

--- src/shared/constants/apiURL.ts
+++ src/shared/constants/apiURL.ts
@@ -1,6 +1,7 @@
 export const BASE_URL = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL
 
 export const API_URL = {
+  USERS : 'users',
   USERS_LOGIN: 'users/login',
   USERS_SIGNUP: 'users/signup',
   USERS_PROFILE: 'users/profile',
