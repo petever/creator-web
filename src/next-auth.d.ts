650c997d2b262c967dc13233abf7e79aaa0f6053
김승훈 <rlatmdgns94@naver.com>
2024-12-16 22:37:21 +0900

    feat: d.ts 설정

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -3,8 +3,11 @@ import { DefaultSession } from 'next-auth'
 declare module 'next-auth' {
   interface User {
     accessToken: string
+    refreshToken: string
   }
+
   interface Session extends DefaultSession {
     accessToken: string
+    refreshToken: string
   }
 }
