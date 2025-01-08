c6c696609b06d8a572cb9ee9d0b261a6acbd0846
김승훈 <rlatmdgns94@naver.com>
2025-01-08 18:26:53 +0900

    test: mobile login

--- app/api/auth/mobile/route.ts
+++ app/api/auth/mobile/route.ts
@@ -1,8 +1,9 @@
 import { type NextRequest, NextResponse } from 'next/server'
+import { redirect } from 'next/navigation'
 
 export function GET(request: NextRequest, response: NextResponse) {
   const searchParams = request.nextUrl.searchParams
   const query = searchParams.get('code')
 
-  return window.location.replace(`shshsk://oauth?code=${query}`)
+  redirect(`shshsk://oauth?code=${query}`)
 }
